fc68d38b7064d5d548eb33f27886da1a44a3e74b
MAHESH KUMAR V C <info@vcmkcybersec.tech>
2021-04-20 13:03:28 +0530

    first commit

---
+++ feeds.py
@@ -0,0 +1,20 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse_lazy
+from django.urls import reverse
+from .models import Post
+
+
+class LatestPostsFeed(Feed):
+    title = 'VCMKCYBERSEC Tutorials'
+    link = reverse_lazy('blog:post_list')
+    description = 'New Post of my Blog'
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords(item.body, 30)
